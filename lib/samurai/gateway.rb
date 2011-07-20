513b446d34a04eebec5f241a6b7ad798990e9ce3
Graeme Rouse <graeme@arizonabay.com>
2011-07-19 21:10:37 -0700

    implemented capture, void and credit. generalized creation of a transaction xml payload and added some rdoc comments

--- lib/samurai/gateway.rb
+++ lib/samurai/gateway.rb
@@ -1,27 +1,47 @@
 class Samurai::Gateway < Samurai::Base
   
+  # Returns the default gateway specified by Samurai.gateway_token if you passed it into Samurai.setup_site.
   def self.the_gateway
     Samurai::Gateway.new(:id => Samurai.gateway_token)
   end
   
-  def purchase(payment_method_token, amount, descriptor)
+  # Convienince method that calls the purchase method on the defalt gateway.
+  def self.purchase(*args)
+    the_gateway.purchase(*args)
+  end
+
+  # Convienince method that calls the authorize method on the defalt gateway.
+  def self.authorize(*args)
+    the_gateway.authorize(*args)
+  end
+  
+  # Convienience method to authorize and capture a payment_method for a particular amount in one transaction. 
+  # Parameters:
+  # +payment_method_token+:: token identifying the payment method to authorize
+  # +amount+:: amount to authorize
+  # +descriptor+:: optional descriptor for the transaction
+  # +custom+:: optional custom data, this data does not get passed to the gateway, it is stored within samurai.feefighters.com only
+  # Returns a Samurai::Transaction containing the gateway's response.
+  def purchase(payment_method_token, amount, descriptor = nil, custom = nil)
+    transaction = Samurai::Transaction.transaction_payload(:payment_method_token => payment_method_token, :amount => amount, :descriptor => descriptor, :custom => custom)
     # send a purchase request
-    resp = post(:purchase, 
-      :transaction => {
-        :amount => amount, 
-        :type => 'purchase', 
-        :payment_method_token => payment_method_token, 
-        :currency_code => 'USD',
-        :descriptor => descriptor
-      }
-    )
+    resp = post(:purchase, {}, transaction)
     # return the response, wrapped in a Samurai::Transaction
     Samurai::Transaction.new.load_attributes_from_response(resp)
   end
 
-  # TODO implement this functionality
-  # def authorize
-  #   post(:authorize)
-  # end
+  # Authorize a payment_method for a particular amount. 
+  # Parameters:
+  # +payment_method_token+:: token identifying the payment method to authorize
+  # +amount+:: amount to authorize
+  # +descriptor+:: optional descriptor for the transaction
+  # +custom+:: optional custom data, this data does not get passed to the gateway, it is stored within samurai.feefighters.com only
+  # Returns a Samurai::Transaction containing the gateway's response.
+  def authorize(payment_method_token, amount, descriptor = nil, custom = nil)
+    transaction = Samurai::Transaction.transaction_payload(:payment_method_token => payment_method_token, :amount => amount, :descriptor => descriptor, :custom => custom)
+    resp = post(:authorize, {}, transaction)
+    # return the response, wrapped in a Samurai::Transaction
+    Samurai::Transaction.new.load_attributes_from_response(resp)
+  end
   
 end
--- lib/samurai/transaction.rb
+++ lib/samurai/transaction.rb
@@ -1,25 +1,51 @@
 class Samurai::Transaction < Samurai::Base
-
+  
   include Samurai::CacheableByToken
   
-  def id
-    self.reference_id
-  end
-  
-  def token
+  # Alias for transaction_token
+  def id # :nodoc:
     transaction_token
   end
   
-  def capture
-    
+  # Captures an authorization. Optionally specify an +amount+ to do a partial capture of the initial
+  # authorization. The default is to capture the full amount of the authroization.
+  def capture(amount = nil)
+    resp = post(:capture, {}, self.class.transaction_payload(:amount => amount || self.amount))
+    # return the response, wrapped in a Samurai::Transaction
+    Samurai::Transaction.new.load_attributes_from_response(resp)
   end
   
+  # Void this transaction. If the transaction has not yet been captured and settled it can be voided to 
+  # prevent any funds from transfering. 
   def void
-    
+    resp = post(:void, {}, self.class.transaction_payload())
+    # return the response, wrapped in a Samurai::Transaction
+    Samurai::Transaction.new.load_attributes_from_response(resp)
   end
   
-  def credit
-    
+  # Create a credit or refund against the original transaction.
+  # Optionally accepts an +amount+ to credit, the default is to credit the full 
+  # value of the original amount
+  def credit(amount = nil)
+    resp = post(:credit, {}, self.class.transaction_payload(:amount => amount || self.amount))
+    # return the response, wrapped in a Samurai::Transaction
+    Samurai::Transaction.new.load_attributes_from_response(resp)
   end
-
+  
+  private
+  
+  # Builds an xml payload that represents the transaction data to submit to samuari.feefighters.com
+  def self.transaction_payload(options = {})
+    {
+      :amount => options[:amount],
+      :type => options[:type],
+      :payment_method_token => options[:payment_method_token],
+      :currency_code => options[:currency_code] || (options[:payment_method_token] && 'USD'), # currency code is only required for payloads that include the PMT
+      :descriptor => options[:descriptor],
+      :custom => options[:custom]
+    }.
+      reject{ |k,v| v.nil? }.
+      to_xml(:skip_instruct => true, :root => 'transaction', :dasherize => false)
+  end
+  
 end
