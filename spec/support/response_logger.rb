2179a205433c29370e4d71e7732a5cf53ce59b32
Joshua Krall <joshuakrall@pobox.com>
2011-08-16 04:16:05 -0500

    Revised formatting of request-response log file

--- spec/support/response_logger.rb
+++ spec/support/response_logger.rb
@@ -7,28 +7,33 @@ class ResponseLogger
     @io = io
     @io.puts "<!doctype html>"
     @io.puts "<html>"
+    @io.puts '<div class="wrapper">'
   end
 
   def close!
+    @io.puts '</div>'
     @io.puts "</html>"
   end
 
   def log(name, request, response)
-    @io.puts "<div class='example'>"
+    @io.puts "<article class='example span-10 prepend-1'>"
     @io.puts "  <h3>#{h name}</h3>"
-    @io.puts "  <div class='request'>"
-    @io.puts "    <p>"
-    @io.puts "      <span class='method'>#{h request[:method].to_s.upcase}</span>"
-    @io.puts "      <span class='path'>#{h request[:path]}</span>"
-    @io.puts "    </p>"
-    @io.puts "    <p class='headers'>#{h request[:headers]}</p>"
-    @io.puts "    <pre class='prettyprint lang-xml'><code>#{h request[:body]}</code></pre>" if request[:body]
-    @io.puts "  </div>"
-    @io.puts "  <div class='response'>"
-    @io.puts "    <p class='status'>Status Code: <span>#{h response.code}</span></p>"
+    @io.puts '  <div class="code http-request"><em class="lang">HTTP Request</em>'
+    @io.puts "    <pre>#{h request[:method].to_s.upcase} #{h request[:path]}</pre>"
+    @io.puts '  </div>'
+    @io.puts "  <p class='headers'>#{h request[:headers]}</p>"
+    if request[:body]
+      @io.puts '  <div class="code xml">'
+      @io.puts '    <em class="lang">XML</em>'
+      @io.puts "    <pre class='prettyprint lang-xml'>#{h request[:body]}</pre>"
+      @io.puts '  </div>'
+    end
+    @io.puts '  <div class="code http-response">'
+    @io.puts "    <em class='lang'>HTTP Response: #{h response.code}</em>"
     @io.puts "    <pre class='prettyprint lang-xml'><code>#{h response.body}</code></pre>"
     @io.puts "  </div>"
-    @io.puts "</div>"
+    @io.puts "</article>"
+    @io.puts "<hr>"
   end
 
 end
@@ -41,6 +46,18 @@ module ResponseLoggerHelper
   end
 
   def log_request_response! request, response
+    def request.[](v)
+      case v
+      when :method
+        return self.method
+      when :path
+        return self.path
+      when :headers
+        return self.to_hash
+      else
+        super
+      end
+    end
     @logger.log example.full_description.sub(/generate documentation/, '').titleize,
                 request, response
   end
