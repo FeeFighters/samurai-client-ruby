36c94da0048436b8d6f42c06f41ec5377c1d57a4
Joshua Krall <joshuakrall@pobox.com>
2011-08-17 23:48:35 -0500

    Move Views into Rails module

---
+++ lib/samurai/rails.rb
@@ -0,0 +1,7 @@
+module Samurai
+  module Rails
+  end
+end
+
+require 'samurai/rails/views'
+require 'samurai/rails/helpers'
---
+++ lib/samurai/rails/views.rb
@@ -0,0 +1,24 @@
+require 'pathname'
+
+module Samurai::Rails
+  class Views
+    class << self
+
+      PARTIALS = [ 'payment_method_form', 'transaction_form', 'errors', 'transaction' ]
+
+      PARTIALS.each do |partial|
+        define_method partial do |attrs|
+          attrs ||= {}
+          {:file=>send("#{partial}_file"), :locals=>attrs}
+        end
+        define_method "#{partial}_file" do
+          Pathname.new(__FILE__).dirname.join('../../..', 'app', 'views', 'application', "_#{partial}.html.erb").to_s
+        end
+        define_method "#{partial}_html" do
+          File.read(send partial)
+        end
+      end
+
+    end
+  end
+end
