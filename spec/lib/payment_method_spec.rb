2edcd1eb27df3bf4e19ac47defc839622a237866
Joshua Krall <joshuakrall@pobox.com>
2011-12-17 04:24:48 -0600

    Adding additional spec for S2S#update

--- spec/lib/payment_method_spec.rb
+++ spec/lib/payment_method_spec.rb
@@ -106,6 +106,19 @@ describe "PaymentMethod" do
 
   describe 'S2S #update' do
     before do
+      @params = {
+        :first_name   => "FirstNameX",
+        :last_name    => "LastNameX",
+        :address_1    => "123 Main St.X",
+        :address_2    => "Apt #3X",
+        :city         => "ChicagoX",
+        :state        => "IL",
+        :zip          => "10101",
+        :card_number  => "5454-5454-5454-5454",
+        :cvv          => "456",
+        :expiry_month => '05',
+        :expiry_year  => "2016",
+      }
       @pm = Samurai::PaymentMethod.find create_payment_method(default_payment_method_params)[:payment_method_token]
     end
     it 'should be successful' do
@@ -121,6 +134,27 @@ describe "PaymentMethod" do
         pm.state.should       == @params[:state]
         pm.zip.should         == @params[:zip]
         pm.last_four_digits.should == @params[:card_number][-4, 4]
+        pm.expiry_month.should  == @params[:expiry_month].to_i
+        pm.expiry_year.should   == @params[:expiry_year].to_i
+      end
+    end
+    it 'should be successful preserving sensitive data' do
+      _params = @params.merge({
+        :card_number => '****************',
+        :cvv => '***',
+      })
+      @pm.update_attributes _params
+      Samurai::PaymentMethod.find(@pm.token).tap do |pm|
+        pm.is_sensitive_data_valid.should be_true
+        pm.is_expiration_valid.should be_true
+        pm.first_name.should  == @params[:first_name]
+        pm.last_name.should   == @params[:last_name]
+        pm.address_1.should   == @params[:address_1]
+        pm.address_2.should   == @params[:address_2]
+        pm.city.should        == @params[:city]
+        pm.state.should       == @params[:state]
+        pm.zip.should         == @params[:zip]
+        pm.last_four_digits.should == '1111'
         pm.expiry_month.should  == @params[:expiry_month].to_i
         pm.expiry_year.should   == @params[:expiry_year].to_i
       end
