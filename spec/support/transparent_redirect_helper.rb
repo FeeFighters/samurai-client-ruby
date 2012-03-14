0156ed1a1bba533084e83f0f44f4bab5d2c54ca2
Joshua Krall <joshuakrall@pobox.com>
2012-03-14 16:04:28 -0700

    Fixing create_payment_method spec helper, for non-ssl staging env

--- spec/support/transparent_redirect_helper.rb
+++ spec/support/transparent_redirect_helper.rb
@@ -5,7 +5,7 @@ module TransparentRedirectHelper
 
   def create_payment_method(params = {}, options={})
     url = Samurai.site + 'payment_methods'
-    url.sub! %r{https://}, "https://#{Samurai.merchant_key}:#{Samurai.merchant_password}@"
+    url.sub! %r{://}, "://#{Samurai.merchant_key}:#{Samurai.merchant_password}@"
 
     uri = URI.parse url
     req = Net::HTTP::Post.new uri.path
@@ -13,7 +13,7 @@ module TransparentRedirectHelper
     req.basic_auth uri.user, uri.password
 
     res = Net::HTTP.new(uri.host, uri.port)
-    res.use_ssl = true
+    res.use_ssl = true if url =~ /https/
 
     puts "---------------------------------------"
     puts "-- #{uri.inspect} "
